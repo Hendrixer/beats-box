25bb58f45779f7e4cd5dba3eb4be4610fb63a9e4
Hendrixer <willscottmoss@gmail.com>
2014-05-21 01:08:05 -0700

    [add beats api service]

---
+++ www/app/app.js
@@ -0,0 +1,22 @@
+angular.module('beats', ['ionic', 'app'])
+
+.run(function($ionicPlatform, $state, localStorageService, BeatsApi) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+    var token = localStorageService.get('beats-token');
+    if(!token) {
+      $state.go('app.login');
+    } else {
+      BeatsApi.init(token);
+      $state.go('app.feed');
+    }
+  });
+});
